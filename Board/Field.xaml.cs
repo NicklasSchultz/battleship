27f19cfc4ca1b95d8bb81d6b5db8714000f89a3c
unknown <nicklas_schultz@hotmail.com>
2013-11-14 15:39:16 +0100

    Init

---
+++ Board/Field.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Battleship.Ships;
+namespace Battleship.Board
+{
+    /// <summary>
+    /// Interaction logic for Field.xaml
+    /// </summary>
+    public partial class Field : UserControl
+    {
+
+        public Ship s{get;set;}
+
+        public Field()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            b.Background = Brushes.LightGreen;
+        }
+    }
+}
