5dd0fadb8ae720fa6f1d196b86fb8413341c5bf0
Eric Lundmark <erilu506@gmail.com>
2013-12-10 14:23:34 +0100

    added some more tests

--- SavedGame.cs
+++ SavedGame.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Battleship
 {
-    class SavedGame
+    public class SavedGame
     {
-        private String name;
-        private Player player1;
-        private Player player2;
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
 
         public SavedGame(String name, Player player1, Player player2)
         {
-            this.name = name;
-            this.player1 = player1;
-            this.player2 = player2;
+            this.Name = name;
+            this.Player1 = player1;
+            this.Player2 = player2;
         }
     }
 }
